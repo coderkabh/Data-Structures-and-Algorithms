a084d8a5153fa218ffcc8fd68cb46e6b70590f23
Kanishk Bharadwaj <kanishkbharadwaj333@gmail.com>
2020-10-29 18:16:56 +0530

    InsertionSort.java code done

--- src/sorting/InsertionSort.java
+++ src/sorting/InsertionSort.java
@@ -1,4 +1,36 @@
 package sorting;
 
+import java.util.Scanner;
+
 public class InsertionSort {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the size of the array");
+        int size = scan.nextInt();
+        int[] arr = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            arr[i] = scan.nextInt();
+        }
+
+        InsertionSort insertionSort = new InsertionSort();
+        insertionSort.insertionSort(arr);
+
+        for (int element: arr){
+            System.out.print(element+" ");
+        }
+    }
+
+    public void insertionSort(int[] arr){
+        for(int i=1;i<arr.length;i++){
+            int current = arr[i];
+            int j = i-1;
+
+            while (j>=0 && arr[j] > arr[i]){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = current;
+        }
+    }
 }
