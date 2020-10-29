dac588d4d3b66e4df322ffa17fd27ddc420c9574
Kanishk Bharadwaj <kanishkbharadwaj333@gmail.com>
2020-10-29 22:13:04 +0530

    fixed a bug in SelectionSort

--- src/sorting/SelectionSort.java
+++ src/sorting/SelectionSort.java
@@ -24,9 +24,11 @@ public class SelectionSort {
         for (int i = 0; i < arr.length-1; i++){
 
             int min = i;
-
-            for(int j = i+1 ; j<arr.length;j++){
+            for(int j = i+1 ; j<arr.length; j++){
                 if(arr[min]>arr[j]){
+                    min = j;
+
+
                     int temp = arr[min];
                     arr[min] = arr[i];
                     arr[i] = temp;
