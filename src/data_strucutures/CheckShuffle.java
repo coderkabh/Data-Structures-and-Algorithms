e955e0285f8c9c2661457990f3aca6c8c30fbb5f
Kanishk Bharadwaj <kanishkbharadwaj333@gmail.com>
2020-11-04 23:24:41 +0530

    code done for CheckShuffle.java

--- src/data_strucutures/CheckShuffle.java
+++ src/data_strucutures/CheckShuffle.java
@@ -1,4 +1,22 @@
 package data_strucutures;
 
 public class CheckShuffle {
+    public static void main(String[] args) {
+        String a = "ABCQY";
+        String b = "DEFZL";
+        String r = "ADBCEFQYZL";
+
+        if (r.length() != a.length() + b.length()) {
+            System.out.println("No");
+        }
+        int i = 0, j = 0, k = 0;
+        while (k < r.length()) {
+            if (i < a.length() && a.charAt(i) == r.charAt(k)) i++;
+            else if (j < b.length() && b.charAt(j) == r.charAt(k)) j++;
+            else System.out.println("No");
+            k++;
+        }
+        if (i + j == r.length()) System.out.println("Yes");
+        else System.out.println("No");
+    }
 }
